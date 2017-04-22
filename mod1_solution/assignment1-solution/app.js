65c7bcd86b2f4f1c9c494033493112dd0b8f841b
ankit.kejriwal <ankitkejriwaal@gmail.com>
2017-04-23 02:42:43 +0530

    mod1_solution

--- mod1_solution/assignment1-solution/app.js
+++ mod1_solution/assignment1-solution/app.js
@@ -6,20 +6,38 @@
     LunchCheckController.$inject = ['$scope'];
     
     function LunchCheckController($scope){
+
         $scope.checkNumberOfItem = function(){
             var count = 0;
-            var numberOfitem = $scope.item.split(',');
-            for(var i =0; i<numberOfitem.length;i++){
-                if(numberOfitem[i] !== "")
-                    count++;
-            }
-            if(count === 0)
-                $scope.message= "Please enter data first";
-            else if(count <=3)
+            if($scope.item != undefined){
+                var numberOfitem = $scope.item.split(',');
+                for(var i =0; i<numberOfitem.length;i++){
+                    if(numberOfitem[i].trim().length !=0){
+                        count++;
+                    }
+                } 
+                if(count === 0){
+                    $scope.message= "Please enter data first";
+                    document.getElementById('msdDiv').style.color="red";
+                    document.getElementById('lunch-menu').style.borderColor="red";
+                }
+               else if(count <=3){
                 $scope.message = "Enjoy!";
-            else
-                $scope.message = "Too much!";
-
+                document.getElementById('msdDiv').style.color="green";
+                document.getElementById('lunch-menu').style.borderColor="green";   
+                }
+                else{
+                    $scope.message = "Too much!";
+                    document.getElementById('msdDiv').style.color="green";
+                    document.getElementById('lunch-menu').style.borderColor="green";
+                }
+                }
+            else{
+                $scope.message= "Please enter data first";
+                $scope.message.fontcolor("red");
+                document.getElementById('msdDiv').style.color="red";
+                document.getElementById('lunch-menu').style.borderColor="red";
+            }
         }
         
     };
